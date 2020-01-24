7e5c4ff8a5954e23cb19e224dd765bd244e0e674
Ramazan <ozeramazan1@gmail.com>
2020-01-24 17:58:03 +0300

    Create day_01.py

---
+++ day_01.py
@@ -0,0 +1,17 @@
+import math
+
+modules = []
+total = fuel = 0
+
+with open('input.txt', 'r') as f: #reading file
+    modules = f.read().splitlines() #copy lines to array 1st = 19115 2nd = 49739
+    #print(modules)
+        
+    for x in modules:
+        mass = int(x)
+        fuel = (math.floor(mass/3))-2
+        total = fuel +total
+    print(total)
+    #calculate every module
+    
+
